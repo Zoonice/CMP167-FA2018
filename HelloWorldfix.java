2b7ad4ea1c278fb383fa9c28e22f3bb90a6048ec
Tesfa <43484797+CallMeTes@users.noreply.github.com>
2018-09-21 20:32:05 -0400

    This is an update file from our "Week_1" homework
    Please review these changes to understand the idea of syntax and semantics

---
+++ HelloWorldfix.java
@@ -0,0 +1,24 @@
+//TESFA GREAVES - CMP 167 - PROFESSOR YANILDA PERALTA RAMOS - M/W 1PM-2:40PM - FA2018
+
+//Write a Java program to print Hello World//
+
+public class HelloWorld
+{
+	//Start to main function//
+	public static void main(String args[])
+			
+	{
+		
+		//Use lnbuilt function to print//
+		System.out.println("Hello,");
+							("World");
+
+//C:\Users\TESFA\Desktop\CMP167\Week 1\Exercises>javac helloworldfix.java
+//helloworldfix.java:14: error: not a statement
+//                            ("World");
+//                            ^
+//1 error
+
+
+	}//End main//
+}//Close Class//
